7f2fa826d5d83d3fa4f49a5a750f8a4821c8aaed
Gabor_Vajda <Gabor_Vajda@EPHUBUDW0124.budapest.epam.com>
2015-05-05 17:55:56 +0200

    first class/test

---
+++ src/test/java/problem/LinkedListCircleTest.java
@@ -0,0 +1,13 @@
+package problem;
+
+import org.testng.annotations.Test;
+
+public class LinkedListCircleTest {
+  @Test
+  public void create() {
+	  //GIVEN
+	  //WHEN
+	  //THEN
+	  new LinkedListCircle();
+  }
+}
