65e7b4d420fb322ba144c56e0abd2b45450233d8
Gabor_Vajda <Gabor_Vajda@EPHUBUDW0124.budapest.epam.com>
2015-05-13 17:00:49 +0200

    simple implementation

--- src/main/java/problem/nailingplanks/NailingPlanks.java
+++ src/main/java/problem/nailingplanks/NailingPlanks.java
@@ -1,10 +1,26 @@
 package problem.nailingplanks;
 
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class NailingPlanks {
 
-	public void nailsNeeded(Object object, Object object2, Object object3) {
-		// TODO Auto-generated method stub
-		
+	public int nailsNeeded(int[] a, int[] b, int[] c) {
+		if (a == null || b == null || c == null) {
+			throw new IllegalArgumentException();
+		}
+		NavigableSet<Integer> usedNails = new TreeSet<Integer>();
+		for (int i = 0; i < a.length; i++) {
+			Integer nailCandidate = usedNails.ceiling(a[i]);
+			boolean nailFound = nailCandidate != null ? nailCandidate < b[i] : false;
+			if (!nailFound) {
+				int nailToAdd = 0;
+				while (nailToAdd < a[i] || b[i] < nailToAdd) {
+					nailToAdd = c[usedNails.size()];
+					usedNails.add(nailToAdd);
+				}
+			}
+		}
+		return usedNails.size();
 	}
-
 }
--- src/test/java/problem/nailingplanks/NailingPlanksTest.java
+++ src/test/java/problem/nailingplanks/NailingPlanksTest.java
@@ -1,5 +1,6 @@
 package problem.nailingplanks;
 
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,5 +15,16 @@ public class NailingPlanksTest {
 	@Test(expectedExceptions=IllegalArgumentException.class)
 	public void callWithNullsExpectedIAE(){
 		underTest.nailsNeeded(null,null,null);
+	}
+	@Test
+	public void callWithExerciseCase(){
+		int result = underTest.nailsNeeded(new int[]{1,4,5,8},new int[]{4,5,9,10},new int[]{4,6,7,10,2});
+		assertEquals(result,4);
+	}
+	
+	@Test
+	public void callWithComplexCase(){
+		int result = underTest.nailsNeeded(new int[]{1,4,5,8,1},new int[]{4,5,9,10,2},new int[]{4,6,7,10,2});
+		assertEquals(result,5);
 	}
 }
